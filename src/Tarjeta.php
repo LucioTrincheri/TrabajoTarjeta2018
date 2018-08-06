80133df959e1ee2ac70c5d04d623a54d91ef1c59
Francisco <fraanb.2999@gmail.com>
2018-08-06 16:56:09 -0300

    Cambio , por .

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -7,14 +7,14 @@ class Tarjeta implements TarjetaInterface {
     protected $saldo;
 
     public function recargar($monto) {
-      if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510,15 || $monto == 962,59) {
+      if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510.15 || $monto == 962.59) {
 
-	if($monto == 510,15) {
-		$this->saldo += 81,93;
+	if($monto == 510.15) {
+		$this->saldo += 81.93;
 	}
 
-	if($monto == 962,59) {
-		$this->saldo += 221,58;
+	if($monto == 962.59) {
+		$this->saldo += 221.58;
 	}
 
         $this->saldo += $monto;
