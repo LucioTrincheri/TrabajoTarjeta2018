cda322d766e02deba2bbdadd054de09b71b49377
LucioTrincheri <lucio4852071@gmail.com>
2018-08-29 19:57:20 -0300

    Separadas las franquicias

--- src/Franquicias.php
+++ src/Franquicias.php
@@ -2,10 +2,6 @@
 
 namespace TrabajoTarjeta;
 
-class MedioBoleto extends Tarjeta {
-	protected $valorPasaje = 7.4;
-}
-
 class FranquiciaCompleta extends Tarjeta {
 	protected $valorPasaje = 0;
 }
---
+++ src/MedioBoleto.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace TrabajoTarjeta;
+
+class MedioBoleto extends Tarjeta {
+	protected $valorPasaje = 7.4;
+}
