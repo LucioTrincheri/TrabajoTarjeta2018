f3440a2395c7eebf094120f37350ad3260742621
LucioTrincheri <lucio4852071@gmail.com>
2018-09-10 00:23:03 -0300

    Agrego métodos a la instancia y actualizo los tests con los nuevos atributos

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -12,9 +12,12 @@ class BoletoTest extends TestCase {
 
     public function testSaldoCero() {
         $valor = 14.80;
-        $boleto = new Boleto($valor, NULL, NULL);
-
-        $this->assertEquals($boleto->obtenerValor(), $valor);
+		$colectivo = new Colectivo(NULL, NULL, NULL);
+		$tiempoReal = new Tiempo;
+		$tarjeta = new Tarjeta($tiempoReal);
+		$tarjeta->recargar(20);
+        $boleto = new Boleto($colectivo, $tarjeta);
+        $this->assertEquals($boleto->Abono(), 0);
     }
 
 	public function testViajeSimple() {
@@ -22,10 +25,23 @@ class BoletoTest extends TestCase {
 		$tiempoReal = new Tiempo;
 		$tarjeta = new Tarjeta($tiempoReal);
 		$tarjeta->recargar(20);
-
 		$valor = 14.80;
-        	$boleto = new Boleto($valor, $colectivo, $tarjeta);
-
+        $boleto = new Boleto($colectivo, $tarjeta);
 		$this->assertEquals($boleto->obtenerColectivo(), $colectivo);
+	}
+
+	public function testNuevosAtributos(){
+		$colectivo = new Colectivo(NULL, "142 Verde", NULL);
+		$tiempoReal = new Tiempo;
+		$tarjeta = new Tarjeta($tiempoReal);
+		$tarjeta->recargar(20);
+		$boleto = $colectivo->pagarCon($tarjeta);
+		$this->assertEquals($boleto->Fecha(), $tiempoReal->time());
+		$this->assertEquals($boleto->TipoTarjeta(), "Movi");
+		$this->assertEquals($boleto->SaldoTarjeta(), 5.2);
+		$this->assertEquals($boleto->IDTarjeta(), 0);
+		$this->assertEquals($boleto->LineaColectivo(), "142 Verde");
+		$this->assertEquals($boleto->Abono(), 14.8);
+		$this->assertEquals($boleto->PlusAbonados(), 0);
 	}
 }
