4de46ff0099878f9590954389c2cbee4bda2531c
LucioTrincheri <lucio4852071@gmail.com>
2018-09-09 23:59:24 -0300

    Agrego nuevos atributos

--- src/Boleto.php
+++ src/Boleto.php
@@ -4,18 +4,32 @@ namespace TrabajoTarjeta;
 
 class Boleto implements BoletoInterface {
 
-    protected $valor;
     protected $colectivo;
 	protected $tarjeta;
 
-    public function __construct($valor, $colectivo, $tarjeta) {
-        $this->valor = $valor;
+	protected $fecha;
+	protected $tipoT;
+	protected $saldoT;
+	protected $IDT;
+	protected $lineaC;
+	protected $abono;
+	protected $plusA;
+
+    public function __construct($colectivo, $tarjeta) {
 		$this->colectivo = $colectivo;
 		$this->tarjeta = $tarjeta;
+
+		$this->fecha = date("d/m/Y H:i:s", $tarjeta->getHora());
+		$this->tipoT = $tarjeta->tipoTarjeta();
+		$this->saldoT = $tarjeta->obtenerSaldo();
+		$this->IDT = $tarjeta->getID();
+		$this->lineaC = $colectivo->linea();
+		$this->abono = $tarjeta->ultAbono();
+		$this->plusA = $tarjeta->ultCantPlus();
     }
 
     public function obtenerValor() {
-        return $this->valor;
+        return $this->abono;
     }
 
     public function obtenerColectivo() {
